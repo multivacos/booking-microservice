2123de912024082f127425f335ccb3098140187a
Jonatan Alvarez <alvarez.geek@hotmail.com>
2024-07-14 15:26:59 -0500

    Asociando a config server GIT

--- src/main/java/com/corp/booking_microservice/BookingMicroserviceApplication.java
+++ src/main/java/com/corp/booking_microservice/BookingMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.corp.booking_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookingMicroserviceApplication {
 
 	public static void main(String[] args) {
